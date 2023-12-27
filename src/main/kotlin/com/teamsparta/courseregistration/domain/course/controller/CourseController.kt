50d4deff48f12dbc400f924269885bfa10e0a3a2
JEON <jwodn123@naver.com>
2023-12-27 14:49:10 +0900

    feat: 인증/인가 구현하기

--- src/main/kotlin/com/teamsparta/courseregistration/domain/course/controller/CourseController.kt
+++ src/main/kotlin/com/teamsparta/courseregistration/domain/course/controller/CourseController.kt
@@ -4,17 +4,26 @@ import com.teamsparta.courseregistration.domain.course.dto.CourseResponse
 import com.teamsparta.courseregistration.domain.course.dto.CreateCourseRequest
 import com.teamsparta.courseregistration.domain.course.dto.UpdateCourseRequest
 import com.teamsparta.courseregistration.domain.course.service.CourseService
-import com.teamsparta.courseregistration.domain.exception.ModelNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.ErrorResponse
-import org.springframework.web.bind.annotation.*
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/courses")
 @RestController
-class CourseController(private val courseService: CourseService) {
+class CourseController(
+    private val courseService: CourseService
+) {
 
     @GetMapping()
+    @PreAuthorize("hasRole('TUTOR') or hasRole('STUDENT')")
     fun getCourseList(): ResponseEntity<List<CourseResponse>> {
         return ResponseEntity
             .status(HttpStatus.OK)
@@ -22,6 +31,7 @@ class CourseController(private val courseService: CourseService) {
     }
 
     @GetMapping("/{courseId}")
+    @PreAuthorize("hasRole('TUTOR') or hasRole('STUDENT')")
     fun getCourse(@PathVariable courseId: Long): ResponseEntity<CourseResponse> {
         return ResponseEntity
             .status(HttpStatus.OK)
@@ -29,6 +39,7 @@ class CourseController(private val courseService: CourseService) {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('TUTOR')")
     fun createCourse(@RequestBody createCourseRequest: CreateCourseRequest): ResponseEntity<CourseResponse> {
         return ResponseEntity
             .status(HttpStatus.CREATED)
@@ -36,6 +47,7 @@ class CourseController(private val courseService: CourseService) {
     }
 
     @PutMapping("/{courseId}")
+    @PreAuthorize("hasRole('TUTOR')")
     fun updateCourse(
         @PathVariable courseId: Long,
         @RequestBody updateCourseRequest: UpdateCourseRequest
@@ -46,6 +58,7 @@ class CourseController(private val courseService: CourseService) {
     }
 
     @DeleteMapping("/{courseId}")
+    @PreAuthorize("hasRole('TUTOR')")
     fun deleteCourse(@PathVariable courseId: Long): ResponseEntity<Unit> {
         courseService.deleteCourse(courseId)
         return ResponseEntity
--- src/main/kotlin/com/teamsparta/courseregistration/infra/security/SecurityConfig.kt
+++ src/main/kotlin/com/teamsparta/courseregistration/infra/security/SecurityConfig.kt
@@ -3,15 +3,21 @@ package com.teamsparta.courseregistration.infra.security
 import com.teamsparta.courseregistration.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.AccessDeniedHandler
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class SecurityConfig(
-    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val authenticationEntrypoint: AuthenticationEntryPoint,
+    private val accessDeniedHandler: AccessDeniedHandler
 ) {
 
     @Bean
@@ -27,11 +33,13 @@ class SecurityConfig(
                     "/swagger-ui/**",
                     "/v3/api-docs/**",
                 ).permitAll()
-                    // 위 URI를 제외하곤 모두 인증이 되어야 함.
                     .anyRequest().authenticated()
             }
-            // 기존 UsernamePasswordAuthenticationFilter 가 존재하던 자리에 JwtAuthenticationFilter 적용
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling{
+                it.authenticationEntryPoint(authenticationEntrypoint)
+                it.accessDeniedHandler(accessDeniedHandler) // 추가!!
+            }
             .build()
     }
 
